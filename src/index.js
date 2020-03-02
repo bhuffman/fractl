d8188eb71cc02ba49079361980f1af2a6e6039e2
bhuffman <1stdraft@gmail.com>
2020-03-02 13:50:43 -0700

    Library Update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { Auth0Provider } from "./react-auth0-spa";
-import history from "./utils/history";
+import React from "react"
+import ReactDOM from "react-dom"
+import "./styles/index.css"
+import App from "./App"
+import * as serviceWorker from "./serviceWorker"
+import { Auth0Provider } from "./react-auth0-spa"
+import history from "./utils/history"
+import { resetContext, getContext } from "kea"
+import { Provider } from "react-redux"
 
 const onRedirectCallback = appState => {
   history.push(
     appState && appState.targetUrl
       ? appState.targetUrl
       : window.location.pathname
-  );
-};
+  )
+}
+
+resetContext({
+  createStore: {},
+  plugins: []
+})
 
 ReactDOM.render(
   <Auth0Provider
@@ -21,12 +28,14 @@ ReactDOM.render(
     redirect_uri={window.location.href}
     onRedirectCallback={onRedirectCallback}
   >
-    <App />
+    <Provider store={getContext().store}>
+      <App />
+    </Provider>
   </Auth0Provider>,
   document.getElementById("root")
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
